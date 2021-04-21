bb47e94c7258c5110f072f4416edf8cce171197c
Leonardo Fragozo <lqfragozo@gmail.com>
2021-04-21 14:20:07 -0300

    Try to implement merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,22 @@
+def merge_sort(arr)
+  if arr.size <= 1
+    arr
+  else
+    middle = (arr.size / 2).floor
+    left = merge_sort(arr[0..middle - 1])
+    right = merge_sort(arr[middle..arr.size])
+    merge(left, right)
+  end
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.size], right)
+  else
+    [right.first] + merge(left, right[1..right.size])
+  end
+end
