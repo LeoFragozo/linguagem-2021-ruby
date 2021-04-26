e62bd8a2da7a00e7a0805c7720c6a0ff3d394736
Leonardo Fragozo <lqfragozo@gmail.com>
2021-04-26 19:54:14 -0300

    Jump search todo fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,18 @@
-array = [1,2,5,3,4]
-
-def bubble_sort(array)
-  array_size = array.length
-  return array if array.size <= 1
+def bubble_sort(unsort_array)
+  unsort_array_size = unsort_array.length
+  return unsort_array if unsort_array.size <= 1
 
   loop do
     swap = false
+    (unsort_array_size - 1).times do |i|
+      next unless unsort_array[i] > unsort_array[i + 1]
 
-    (array_size - 1).times do |i|
-      if array[i] > array[i+1]
-        array [i], array[i + 1] = array[i + 1], array[i]
-        swap = true
-      end
+      unsort_array [i], unsort_array[i + 1] = unsort_array[i + 1], unsort_array[i]
+      swap = true
     end
-
     break unless swap
   end
-
-  array
+  unsort_array
 end
-unsort_array = [75,23,68,54,03,54,93,31,18,22]
+unsort_array = [75, 23, 68, 54, 3, 54, 93, 31, 18, 22]
 p bubble_sort(unsort_array)
--- jump-search.rb
+++ jump-search.rb
@@ -1,29 +1,30 @@
 def jump_search(arr, value)
   n = arr.size # 9
   step = Math.sqrt(n) # 3
+
   prev = 0
-  
-  while arr[[step,n].min] < value do #3,9,min (3) < 105
+
+  while arr[[step, n].min] < value # 3,9,min (3) < 105
     prev = step # prev = 3
-    step += Math.sqrt(n)  # step = 3 + 3 ->  6 
-    return -1 if prev >= n # 3 >= 9 (false) 
+    step += Math.sqrt(n) # step = 3 + 3 ->  6
+    return -1 if prev >= n # 3 >= 9 (false)
   end
 
-  while arr[prev] < value do # 6 < 105
-    prev += 1  #  0 += 1
-
+  while arr[prev] < value # 6 < 105
+    prev += 1 #  0 += 1
     return -1 if prev == [step, n].min # retorna -1 se 125 == (3,9).min = (false)
   end
 
-  if arr[prev] == value  #se o arr[3] == 125 
-    return prev.to_i   =  #return 0_to_is
+  if arr[prev] == value # se o arr[3] == 125
+    return prev.to_i   # return 0_to_is
+  end
+
+  -1
 end
 
-  return -1
-end
-
-arr = [6, 14, 58, 93, 105,121,138,145,153]
+arr = [6, 14, 58, 93, 105, 121, 138, 145, 153]
 value = 105
 
 result = jump_search(arr, value)
 
+puts result.negative ? "Número '#{value}' Não encontrado no array" : "Número '#{value}' encontrado no index #{result}"
