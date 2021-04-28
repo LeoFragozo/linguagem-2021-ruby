fb52c508805f36eda027a61b75c13cbc4a7772cc
Leonardo Fragozo <lqfragozo@gmail.com>
2021-04-28 15:03:22 -0300

    Count char ident

--- count_chars.rb
+++ count_chars.rb
@@ -1,26 +1,25 @@
-def countCharacters (chars)
-  count = {upper:  0, down: 0, number: 0, special: 0}
+def countCharacters(chars)
+  count = { upper: 0, down: 0, number: 0, special: 0 }
 
   chars.each_char do |char|
+    if !char.scan(%r{[!@#$%^&*()_+{}\[\]:;'"/\\?><.,]}).empty?
+      count[:special] += 1
 
-    if !char.scan(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/).empty?
-        count[:special] +=1
-
-    elsif char  !~ /\D/
-        count[:number] +=1
+    elsif char !~ /\D/
+      count[:number] += 1
 
     elsif char == char.upcase
-        count [:upper] +=1
+      count [:upper] += 1
 
     elsif char = char.downcase
-        count [:down] +=1
+      count [:down] += 1
     end
   end
-count
+  count
 end
 
 string = 'abcDEF123!@#fghijlMNO789*()'
-  
+
 countCharacters(string).each do |key, value|
   p "#{key}: #{value}"
 end
